af12344bf6e53e6d5a0d4bf2936315b85f32e348
Vincent Carluer <vcarluer@gmail.com>
2014-01-08 17:10:51 +0100

    ga stat splitted. newgods v0.1

--- js/ga.js
+++ js/ga.js
@@ -1,13 +1,5 @@
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29591998-1']);
-  _gaq.push(['_trackPageview']);
-
   // dom ready
   $(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);    
-
     createNavigation();
   });
 
