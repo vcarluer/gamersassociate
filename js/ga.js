4840fd3161aeaa7b7905331a16ee05153e9aa4b8
Vincent Carluer <vcarluer@gmail.com>
2014-01-08 18:22:24 +0100

    new rpg section, new gods corrections

--- js/ga.js
+++ js/ga.js
@@ -7,7 +7,8 @@
 	var nav = $("#ganav");
 	if (nav) {
 		nav.html('<ul>\
-			<li><a href="videogames.htm">Games</a></li>\
+			<li><a href="videogames.htm">Video Games</a></li>\
+			<li><a href="rpg.htm">RPG</a></li>\
 			<li><a href="blog.htm">Blog</a></li>\
 			<li><a href="articles.htm">Articles</a></li>\
 			<li><a href="about.htm">About</a></li>\
