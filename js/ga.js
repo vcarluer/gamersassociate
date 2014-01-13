c9be1711f92ac850a31fcd9de6031bb4050f1474
Vincent Carluer <vcarluer@gmail.com>
2014-01-13 13:16:19 +0100

    Guillaume feedback

--- js/ga.js
+++ js/ga.js
@@ -7,8 +7,8 @@
 	var nav = $("#ganav");
 	if (nav) {
 		nav.html('<ul>\
-			<li><a href="videogames.htm">Video Games</a></li>\
-			<li><a href="rpg.htm">RPG</a></li>\
+			<li><a href="videogames.htm">Video games</a></li>\
+			<li><a href="rpg.htm">Story games</a></li>\
 			<li><a href="blog.htm">Blog</a></li>\
 			<li><a href="articles.htm">Articles</a></li>\
 			<li><a href="about.htm">About</a></li>\
