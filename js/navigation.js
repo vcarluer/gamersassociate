7f6ba91de716469479ad8038999a634a55497a68
clerc <g-clerc@libertysurf.fr>
2014-01-30 17:07:10 +0100

    update navigation menu

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,12 @@
   // dom ready
   $(function() {
-    createNavigation();
-  });
-
-  function createNavigation() {
-	var nav = $("#ganav");
-	if (nav) {
-		nav.html('<ul>\
+    $("#ganav").html('<ul>\
 			<li><a href="videogames.htm">Video games</a></li>\
 			<li><a href="rpg.htm">Story games</a></li>\
+			</ul><ul>\
 			<li><a href="blog.htm">Blog</a></li>\
 			<li><a href="articles.htm">Articles</a></li>\
 			<li><a href="about.htm">About</a></li>\
 			</ul>');
-	}
-  }
+  });
+
