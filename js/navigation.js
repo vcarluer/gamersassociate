7ae7b711522debee06139c80a70be1d613f13ab8
Vincent Carluer <vcarluer@gmail.com>
2014-05-14 15:58:48 +0200

    workshop page

--- js/navigation.js
+++ js/navigation.js
@@ -4,6 +4,7 @@
 			<li><a href="videogames.htm">Video games</a></li>\
 			<li><a href="rpg.htm">Story games</a></li>\
 			</ul><ul>\
+			<li><a href="workshop.htm">Workshop</a></li>\
 			<li><a href="blog.htm">Blog</a></li>\
 			<li><a href="articles.htm">Articles</a></li>\
 			<li><a href="about.htm">About</a></li>\
